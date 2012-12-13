2c56439c07d78216d43689c42568a56eede1ade1
Alexandr Popolitov <popolit@itep.ru>
2012-12-14 01:44:09 +0400

    Correct linebreak handling in scalars. Plain scalar reading 1st version.

--- cl-yaclyaml-t.lisp
+++ cl-yaclyaml-t.lisp
@@ -25,7 +25,47 @@
 (test-doubly-quoted-scalar
  #?"folded \nto a space,\t\n \nto a line feed, or \t\\\n \\ \tnon-content"
  #?"folded to a space,\nto a line feed, or \t \tnon-content")
-	
+
+;; Testing reading of singly quoted scalars
+
+(defmacro def-ts-macro (name func-name add-args)
+  `(defmacro ,name (str-in char-after str-out &optional comment)
+     `(multiple-value-bind (exp-str-out after-char)
+          (,',func-name
+           (make-string-input-stream ,(strcat str-in char-after))
+           ,@',add-args)
+        (ok (and (equal exp-str-out ,str-out)
+                 (equal after-char ,char-after))
+            ,comment))))
+
+(def-ts-macro test-singly-quoted-scalar yaml-single-quote-reader (#\'))
+
+(test-singly-quoted-scalar
+ #?/here''s to "quotes"'/ #\[ #?"here's to \"quotes\"")
+(test-singly-quoted-scalar
+ #?" 1st non-empty\n\n 2nd non-empty \n\t3rd non-empty '" nil
+ #?" 1st non-empty\n2nd non-empty 3rd non-empty ")
+
+;;; Testing plain scalar reading
+(def-ts-macro test-plain-scalar-reader yaml-plain-scalar-reader (nil))
+(test-plain-scalar-reader
+ #?" 1st non-empty\n\n 2nd non-empty \n\t3rd non-empty : "
+ #\:
+ #?"1st non-empty\n2nd non-empty 3rd non-empty")
+
+;;; Testing anchor and alias-node reader
+
+;(def-ts-macro test-alias-node-reader yaml-alias-node-reader ())
+;(def-ts-macro test-anchor-reader yaml-anchor-reader ())
+
+;; (test-anchor-reader
+;;  #?"asdf" #\space #?"asdf")
+;; (test-anchor-reader
+;;  #?"asdf" #\[ #?"asdf")
+;; (test-alias-node-reader
+;;  #?"asdf" #\space #?"asdf")
+;; (test-alias-node-reader
+;;  #?"asdf" #\[ #?"asdf")
 
 
-
+                    
--- cl-yaclyaml.lisp
+++ cl-yaclyaml.lisp
@@ -4,6 +4,14 @@
 
 ;;; "cl-yaclyaml" goes here. Hacks and glory await!
 
+(defun str-* (n str)
+  (iter (for i from 0 below n)
+        (collect str into res)
+        (finally (return (apply #'strcat res)))))
+
+(defmacro fart (&body args)
+  `(format t ,(strcat (str-* (- (length args) 1) "~a ") "~a~%") ,@args))
+
 ;;; For now we will stick to UTF-8 (basic streams)
 ;;; and will use lispbuilder lexing/YaCC-ing facilities extensively.
 ;;; With other encodings, idea would be to use Babel system.
@@ -126,6 +134,68 @@
          ,then
          ,@(if else `(,else)))))
 
+(defmacro-driver (for var in-non-closing-stream stream
+                      &optional using (reader #'read))
+  (sb-int:with-unique-names (eof)
+    "Like in-stream, but does not close the stream upon finish of iteration."
+    `(,(if generate 'generate 'for) ,var
+       next (let ((it (funcall ,reader ,stream nil ',eof t)))
+              (if (eql it ',eof)
+                  (progn (setf ,var it)
+                         (terminate))
+                  it)))))
+
+(defmacro def-yaml-scalar-reader (name args &body body)
+  `(defun ,name ,args
+     (macrolet ((use-whitespace ()
+                  `(progn (nconcing (nreverse whitespace) into res)
+                          (setf whitespace nil)))
+                (discard-whitespace ()
+                  `(setf whitespace nil))
+                (fold-newlines ()
+                  `(progn
+                     (cond ((equal new-line-count 0) nil)
+                           ((equal new-line-count 1)
+                            (collect #\space into res))
+                           (t (collect (make-string (- new-line-count 1)
+                                                    :initial-element #\newline)
+                                into res)))
+                     (setf new-line-count 0)))
+                (b-break-p (char)
+                  `(or (char= ,char #\newline)
+                       (and (char= ,char #\return)
+                            (if (char= (peek-char nil stream nil t t) #\newline)
+                                (next ,char))))))
+       (macrolet ((process-nws-char (&key discard-leading-whitespace)
+                    `(if begin-of-first-line-p
+                         (progn ,(if discard-leading-whitespace
+                                     `(discard-whitespace)
+                                     `(use-whitespace))
+                                (collect char into res)
+                                (setf begin-of-first-line-p nil))
+                         (if begin-of-line-p
+                             (progn (discard-whitespace)
+                                    (fold-newlines)
+                                    (collect char into res)
+                                    (setf begin-of-line-p nil))
+                             (progn (use-whitespace)
+                                    (collect char into res)))))
+                  (process-b-break-char ()
+                    `(progn (discard-whitespace)
+                            (incf new-line-count)
+                            (setf begin-of-first-line-p nil
+                                  begin-of-line-p t))))
+         (flet ((whitespace-p (char) (or (char= char #\space)
+                                         (char= char #\tab))))
+           (let (whitespace
+                 (begin-of-first-line-p t)
+                 (begin-of-line-p t)
+                 (new-line-count 0))
+             (iter (generate char in-non-closing-stream stream using #'read-char)
+                   (next char)
+                   ;;(format t "~a~%" char)
+                   ,@body)))))))
+
 ;; First we define assoc-list of escapes ...
 (let ((escapes
        `((#\0 . #\nul)
@@ -152,68 +222,125 @@
   (defun get-escape-of (char)
     (assoc char escapes))
   ;; ... and then function, which reads double quoted scalars
-  (defun yaml-double-quote-reader (stream double-quote-char)
-    (macrolet ((use-whitespace ()
-                 `(progn (nconcing (nreverse whitespace) into res)
-                         (setf whitespace nil)))
-               (discard-whitespace ()
-                 `(setf whitespace nil))
-               (fold-newlines ()
-                 `(progn
-                    (cond ((equal new-line-count 0) nil)
-                          ((equal new-line-count 1)
-                           (collect #\space into res))
-                          (t (collect (make-string (- new-line-count 1)
-                                                   :initial-element #\newline)
-                               into res)))
-                    (setf new-line-count 0))))
-      (macrolet ((process-nws-char ()
-                   `(if begin-of-first-line-p
-                        (progn (use-whitespace)
-                               (collect char into res)
-                               (setf begin-of-first-line-p nil))
-                        (if begin-of-line-p
-                            (progn (discard-whitespace)
-                                   (fold-newlines)
-                                   (collect char into res)
-                                   (setf begin-of-line-p nil))
-                            (progn (use-whitespace)
-                                   (collect char into res))))))
-        (let (whitespace
-              (begin-of-first-line-p t)
-              (begin-of-line-p t)
-              (new-line-count 0))
-          (iter (generate char in-stream stream using #'read-char)
-                (next char)
-                ;;(format t "~a~%" char)
-                (until (char= char double-quote-char))
-                (cond
-                  ((or (char= char #\tab) (char= char #\space))
-                   (push char whitespace))
-                  ((char= char #\\)
-                   (if (char= (next char) #\newline)
-                       (progn (use-whitespace)
-                              (setf begin-of-first-line-p nil
-                                    begin-of-line-p t))
-                       (let ((esc (assoc char escapes :test #'char=)))
-                         (if (not esc)
-                             (error "Invalid escape character: ~s ~s"
-                                    char
-                                    (char-code char))
-                             (let ((char (apif #'characterp (cdr esc)
-                                               it
-                                               (funcall it stream))))
-                               (process-nws-char))))))
-                  ((char= char #\newline) (progn (discard-whitespace)
-                                                 (incf new-line-count)
-                                                 (setf begin-of-first-line-p nil
-                                                       begin-of-line-p t)))
-                  (t (process-nws-char)))
-                (finally
-                 (return (apply #'strcat (if begin-of-line-p
-                                             res
-                                             (nconc res whitespace)))))))))))
-                      
+  (def-yaml-scalar-reader yaml-double-quote-reader (stream double-quote-char)
+      (until (char= char double-quote-char))
+    (cond
+      ((whitespace-p char) (push char whitespace))
+      ((char= char #\\)
+       (if (char= (next char) #\newline)
+           (progn (use-whitespace)
+                  (setf begin-of-first-line-p nil
+                        begin-of-line-p t))
+           (let ((esc (assoc char escapes :test #'char=)))
+             (if (not esc)
+                 (error "Invalid escape character: ~s ~s"
+                        char
+                        (char-code char))
+                 (let ((char (apif #'characterp (cdr esc)
+                                   it
+                                   (funcall it stream))))
+                   (process-nws-char))))))
+      ((b-break-p char) (process-b-break-char))
+      (t (process-nws-char)))
+    (finally
+     (return (values (apply #'strcat (if begin-of-line-p
+                                         res
+                                         (nconc res whitespace)))
+                     nil)))))
+        
+(def-yaml-scalar-reader yaml-single-quote-reader (stream single-quote-char)
+  (cond
+    ((char= char single-quote-char)
+     (if (char= (next char) single-quote-char)
+         (collect char into res)
+         (finish)))
+    ((whitespace-p char) (push char whitespace))
+    ((b-break-p char) (process-b-break-char))
+    (t (process-nws-char)))
+  (finally
+   ;;(fart "Farting:" char (char-code char))
+   (return (values (apply #'strcat (if begin-of-line-p
+                                       res
+                                       (nconc res whitespace)))
+                   (if (characterp char) char nil)))))
+
+
+
+;;; Read token-like things
+;; (defmacro def-token-reader (name args terminators)
+;;   `(let ((terminators ',terminators))
+;;      (defun ,name ,args
+;;        (iter (generate char in-stream stream using #'read-char)
+;;         (next char)
+;;         (until (find char terminators :test #'char=))
+;;         (collect char into res)
+;;         (finally (return (values (apply #'strcat res)
+;;                                  (if (characterp char) char nil))))))))
+
+
+;(s-white #\space #\tab)
+;(b-char #\newline #\return)
+;(c-flow-indicator #\[ #\] #\{ #\} #\,)
+
+;; Is triggered by #\& character after whitespace
+;(def-token-reader yaml-anchor-reader (stream)
+;  (s-white b-char c-flow-indicator))
+
+;; Is triggered by #\* character after whitespace
+;(def-token-reader yaml-alias-node-reader (stream)
+;  (s-white b-char c-flow-indicator))
+
+;;; Building block for reading directives
+;(def-token-reader yaml-nws#-token-reader (stream)
+;  (s-white b-char #\#))
+
+;;; Questions to answer:
+;;; 1) what happens, when no characters are present in the stream?
+;;; (should return (nil nil)
+;;; 2) should add passing of invokation character (or, in general, string),
+
+;;; And I really want to sleep, it seems
+
+;(defun yaml-directive-reader (stream)
+;  (iter 
+
+;; Can hold values 'flow-in 'flow-out 'flow-key 'block-key
+(defparameter yaml-context 'flow-out)
+
+(def-yaml-scalar-reader yaml-read-comment (stream prev-char)
+  (fart "Comment:" char)
+  (if (b-break-p char) (finish))
+  (finally (return (values nil nil))))
+
+(def-yaml-scalar-reader yaml-plain-scalar-reader (stream already-read-chars)
+  (fart char)
+  (cond
+    ((whitespace-p char) (push char whitespace))
+    ((b-break-p char) (process-b-break-char))
+    ((char= char #\:) (if (whitespace-p (next char))
+                          ;; KLUDGE - whitespace will be disregarded anyway
+                          (progn (setf char #\:)
+                                 (finish))
+                          (progn (collect #\: into res)
+                                 (collect char into res))))
+    ((char= char #\#) (if whitespace
+                          ;; Maybe we should retain comments in serialization
+                          ;; tree?
+                          (yaml-read-comment stream #\#)
+                          (collect char into res)))
+    ((find char '(#\, #\[ #\] #\{ #\}) :test #'char=)
+     (if (find yaml-context '(flow-in flow-key))
+         (finish)
+         (collect char into res)))
+    (t (process-nws-char :discard-leading-whitespace t)))
+  (finally (return (values (apply #'strcat res)
+                           (if (characterp char) char nil)))))
+    
+
+
 
         
-  
+
+;(defun yaml-reader (stream)
+;  "Central function of all module. Analog of read-token in standard Lisp reader."
+;  ...)
